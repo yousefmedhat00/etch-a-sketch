531218af0d987a145de831bf9d3bfc69413b21bf
Yousef Medhat <148549942+yousefmedhat00@users.noreply.github.com>
2024-02-06 14:19:20 +0200

    Add function that creates a grid depending on user input

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container");
+const userInput = document.querySelector("#size");
 
 let gridSquare = 16;
 
@@ -6,11 +7,28 @@ function makeGrid(){
     gridSize = gridSquare * gridSquare;
     for(i = 0; i < gridSize; i++){
         const square = document.createElement("div");
-
+ 
         container.appendChild(square);
         square.classList.add("square");
         square.style.height = `${600 / gridSquare}px`;
         square.style.width = `${600 / gridSquare}px`;
-    }
-}
-makeGrid();
+    };
+};
+console.log(gridSquare)
+
+document.addEventListener("DOMContentLoaded", makeGrid())
+
+userInput.addEventListener("input", () => {
+    square.remove();
+    gridSquare = userInput.value;
+    console.log(gridSquare)
+});
+
+
+
+
+
+
+
+
+
